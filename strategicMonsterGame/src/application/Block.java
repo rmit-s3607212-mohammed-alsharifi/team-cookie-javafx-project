3aee8859d2317cfe3ed876bb09b8978070068e0a
rmit-s3588237-vinhduy-nguyen <s3588237@student.rmit.edu.au>
2017-05-20 15:22:13 +1000

    Update Block.java

--- strategicMonsterGame/src/application/Block.java
+++ strategicMonsterGame/src/application/Block.java
@@ -1,14 +1,16 @@
 package application;
 
-public class Block extends Cell{
-	
-	public Block (int x , int y)
-	{
-		
-		this.id = "BLOCK";
-		xPosition = x;
-		yPosition = y;	
-	}
-	//test
 
+
+public class Block extends Cell 
+{
+	
+	public Block() 
+	{
+		this.id = "BLOCK";
+		
+	}
+	
+
+	
 }
