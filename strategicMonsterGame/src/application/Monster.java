210e9b73d04023cb29320ebb152a1ea32bea96f6
vinhduy-nguyen <s3588237@student.rmit.edu.au>
2017-05-20 12:20:58 +1000

    monter with random movement

--- strategicMonsterGame/src/application/Monster.java
+++ strategicMonsterGame/src/application/Monster.java
@@ -1,12 +1,91 @@
 package application;
 
+import java.util.Random;
+
 public class Monster extends Cell 
 {
-	public Monster (int x , int y)
+	private static int xPos = 3;
+	private static int yPos = 3;
+	static Random r = new Random();
+	private static int right = 0;
+	private static int left = 1;
+	private static int up =2;
+	private static int down = 3;
+	private static int dir = -1;
+	private static int newY;
+	private static int newX;
+	
+	
+	public Monster(int Xinput , int Yinput) //Hero is the return type here. This is a constructor I'm pretty sure.
 	{
 		this.id = "MONSTER";
-		xPosition = x;
-		yPosition = y;	
+		int xPos = Xinput;
+		int yPos = Yinput;
 	}
+	
+	
+	
+	
+		//Encapsulation time!
+		//Be mindful as I go: That strange hero method(?) 
+		//above may just be representative of position.
+	
+	
+	public static int getxPos()
+	{
+		return xPos;
+	}
+	
+	public static int getyPos()
+	{
+		return yPos;
+	}
+	
+	public static void setxpos(int newx)
+	{
+		xPos = newx;
+	}
+	
+	
+	public static void setypos(int newy)
+	{
+		yPos = newy;
+	}
+
+	static void MonsterMove()
+	{
+		dir= r.nextInt(4);
+		if(dir == right)
+		{
+			newX = (Monster.getxPos()+1);
+			Monster.setxpos(newX);
+		}
+		if(dir==left)
+		{
+			newX = (Monster.getxPos()-1);
+			Monster.setxpos(newX);
+		}
+		if(dir==up)
+		{
+			newY = (Monster.getyPos()-1);
+			Monster.setypos(newY);
+		}
+		if(dir==down)
+		{
+			newY = (Monster.getyPos()+1);
+			Monster.setypos(newY);
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+
+	
+	
+	
+	
 }
-//test
